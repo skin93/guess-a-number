08dd47e2834613c4bba24ad91ae5e483b8743e17
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-15 14:28:14 +0200

    Set button width with Dimensions

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from 'react-native'
 import BaseBodyText from '../components/UI/BaseBodyText'
 
@@ -116,8 +117,9 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans-bold',
   },
   inputContainer: {
-    width: 300,
-    maxWidth: '80%',
+    width: '80%',
+    maxWidth: '95%',
+    minWidth: 300,
     alignItems: 'center',
   },
   buttonContainer: {
@@ -127,7 +129,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 100,
+    // width: 100,
+    width: Dimensions.get('window').width / 4,
   },
   input: {
     width: 50,

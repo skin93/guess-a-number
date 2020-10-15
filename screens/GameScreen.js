ec59e4fa21c93a74977616fc5c9a699519613b3c
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-15 13:38:56 +0200

    Manage pastGuesses as a list

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { StyleSheet, Text, View, Alert } from 'react-native'
+import { StyleSheet, Text, View, Alert, ScrollView } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
 import NumberContainer from '../components/UI/NumberContainer'
@@ -21,11 +21,10 @@ const generateRandomBetween = (min, max, exclude) => {
 }
 
 const GameScreen = (props) => {
-  const [currentGuess, setCurrentGuess] = useState(
-    generateRandomBetween(1, 100, props.userChoice)
-  )
+  const initialGuess = generateRandomBetween(1, 100, props.userChoice)
+  const [currentGuess, setCurrentGuess] = useState(initialGuess)
 
-  const [rounds, setRounds] = useState(0)
+  const [pastGuesses, setPastGuesses] = useState([initialGuess])
   const currentLow = useRef(1)
   const currentHigh = useRef(100)
 
@@ -33,9 +32,9 @@ const GameScreen = (props) => {
 
   useEffect(() => {
     if (currentGuess === userChoice) {
-      onGameOver(rounds)
+      onGameOver(pastGuesses.length)
     }
-  }, [currentGuess, userChoice, onGameOver, rounds])
+  }, [currentGuess, userChoice, onGameOver])
 
   const nextGuessHandler = (direction) => {
     if (
@@ -50,7 +49,7 @@ const GameScreen = (props) => {
     if (direction === 'lower') {
       currentHigh.current = currentGuess
     } else {
-      currentLow.current = currentGuess
+      currentLow.current = currentGuess + 1
     }
     const nextNumber = generateRandomBetween(
       currentLow.current,
@@ -58,7 +57,7 @@ const GameScreen = (props) => {
       currentGuess
     )
     setCurrentGuess(nextNumber)
-    setRounds((curRounds) => curRounds + 1)
+    setPastGuesses((currPastGuesses) => [nextNumber, ...currPastGuesses])
   }
 
   return (
@@ -73,6 +72,13 @@ const GameScreen = (props) => {
           <Ionicons name='md-add' size={24} color='white' />
         </BaseButton>
       </BaseCard>
+      <ScrollView>
+        {pastGuesses.map((guess) => (
+          <View key={guess}>
+            <Text>{guess}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   )
 }
